d3bc634de87b9b3523594ce1d3eda04fd9625a48
NakamuraKenshiro <e1b23061@oit.ac.jp>
2025-11-10 10:39:36 +0900

    DBの初期化処理を実装する

--- src/main/java/oit/is/z2883/kaizi/janken/model/MatchMapper.java
+++ src/main/java/oit/is/z2883/kaizi/janken/model/MatchMapper.java
@@ -1,12 +1,17 @@
 package oit.is.z2883.kaizi.janken.model;
 
 import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface MatchMapper {
 
-  @Select("SELECT id, user1, user2, user1hand, user2hand FROM matches")
+  @Select("SELECT id, user1, user2, user1hand, user2hand isActive FROM matches")
   ArrayList<Match> selectAllMatches();
+
+  @Insert("INSERT INTO matches (user1, user2, user1Hand, user2Hand) VALUES (#{user1}, #{user2}, #{user1Hand}, #{user2Hand});")
+  void insertMatch(Match match);
 }
